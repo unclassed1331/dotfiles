da5361053bf0987754e778ed370b568f303b3032
unclassed1331 <kevinvanden@live.com>
2026-09-17 15:23:08 -0600

    .lua split

--- scripts/toggle-gnote.sh
+++ scripts/toggle-gnote.sh
@@ -2,6 +2,6 @@
 if hyprctl clients -j | jq -e '.[] | select(.class=="org.gnome.Gnote" and (.workspace.name=="special:gnote"))' > /dev/null; then
     hyprctl dispatch 'hl.dsp.workspace.toggle_special("gnote")'
 else
-    gsettings set org.gnome.gnote search-window-splitter-pos 339
+    gsettings set org.gnome.gnote search-window-splitter-pos 90
     gnote &
 fi
